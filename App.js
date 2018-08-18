305ea7bc67726494e95e584090250b5ec47a5c91
Joshua Leung <joshuaycleung@gmail.com>
2018-08-19 00:24:41 +0800

    Add MobX libraries and babel dependencies for issue fixes
    Implement Provider and observer to App.js entry point file
    
    Update babel config file to support decorators

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { createStackNavigator } from "react-navigation";
 import UserListScreen from "./src/screens/User/ListScreen";
 import HomeScreen from "./src/screens/HomeScreen";
+import { Provider, observer } from "mobx-react";
 
 const AppNavigator = createStackNavigator(
   {
@@ -19,8 +20,13 @@ const AppNavigator = createStackNavigator(
   }
 );
 
+@observer
 export default class App extends Component {
   render() {
-    return <AppNavigator />;
+    return (
+      <Provider>
+        <AppNavigator />
+      </Provider>
+    );
   }
 }
