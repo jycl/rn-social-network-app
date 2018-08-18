a8e6f8e2ea30506c1a270056e953d6ec789e2aa0
Joshua Leung <joshuaycleung@gmail.com>
2018-08-19 01:10:27 +0800

    Add UserStore and UserList component to UserListScreen
    Add invocation of loadUserList and pass store user list to UserList

--- src/screens/User/ListScreen.js
+++ src/screens/User/ListScreen.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import UserList from "../../components/UserList";
 import colors from "../../styles/colors";
+import { inject, observer } from "mobx-react";
 
 /**
  * UserListScreen is a screen that renders UI components displaying
@@ -8,11 +10,18 @@ import colors from "../../styles/colors";
  *
  * @author Joshua Leung <joshuaycleung@gmail.com>
  */
+@inject("userStore")
+@observer
 class UserListScreen extends Component {
+  componentDidMount() {
+    this.props.userStore.loadUserList();
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={[styles.header, styles.headerText]}>User list:</Text>
+        <UserList userList={this.props.userStore.filteredUserList} />
       </View>
     );
   }
