710f04beedb0aaff8eabd9e2deacd99b2897fea3
Joshua Leung <joshuaycleung@gmail.com>
2018-08-19 21:11:49 +0800

    Update the nav bar to styling and title (to user name) for each screen

--- App.js
+++ App.js
@@ -7,22 +7,51 @@ import PostCommentScreen from "./src/screens/Post/CommentScreen";
 import PhotoGridScreen from "./src/screens/Photo/GridScreen";
 import { Provider, observer } from "mobx-react";
 import stores from "./src/stores";
+import colors from "./src/styles/colors";
+import fontSize from "./src/styles/fontSize";
+import userStore from "./src/stores/userStore";
 
+//callback to update the navBar header title value
+const navStyleRenderer = () => ({
+  title: `${userStore.selectedUser.name}`
+});
 const AppNavigator = createStackNavigator(
   {
     Home: { screen: HomeScreen },
-    UserList: { screen: UserListScreen },
-    UserDetail: { screen: UserDetailScreen },
-    PostComment: { screen: PostCommentScreen },
-    PhotoGrid: { screen: PhotoGridScreen }
-    /* To include:
-        - PhotoAlbumScreen --> PhotoListScreen
-        - PostHistoryScreen
-        - TodoListScreen
-    */
+    UserList: {
+      screen: UserListScreen,
+      navigationOptions: ({ navigation }) => ({
+        title: `User List`,
+        headerTitleStyle: {
+          fontSize: fontSize.large
+        }
+      })
+    },
+    UserDetail: {
+      screen: UserDetailScreen,
+      navigationOptions: navStyleRenderer
+    },
+    PostComment: {
+      screen: PostCommentScreen,
+      navigationOptions: navStyleRenderer
+    },
+    PhotoGrid: {
+      screen: PhotoGridScreen,
+      navigationOptions: navStyleRenderer
+    }
   },
   {
-    initialRouteName: "UserList"
+    initialRouteName: "UserList",
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: colors.darkestBlue
+      },
+      headerTintColor: colors.lightestBlue,
+      headerTitleStyle: {
+        fontSize: fontSize.medium
+      },
+      headerBackTitle: " "
+    }
   }
 );
 
