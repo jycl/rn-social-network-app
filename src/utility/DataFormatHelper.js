8e34f0010432f4656cf4bd235903d055585dc72d
Joshua Leung <joshuaycleung@gmail.com>
2018-08-20 01:52:13 +0800

    Add getInitialsFromName method to extract initials for detail screen

--- src/utility/DataFormatHelper.js
+++ src/utility/DataFormatHelper.js
@@ -12,3 +12,21 @@ export function formatDataForGrid(data, numberOfCol, fillerObj = {}) {
     return data;
   }
 }
+
+export function getInitialsFromName(name) {
+  if (!name) {
+    return name;
+  }
+  let segments = name.split(" ");
+  let initials = "";
+  for (let i = 0; i < segments.length; i++) {
+    if (!segments[i][0] || segments[i].indexOf(".") > -1) {
+      continue;
+    }
+    initials += segments[i][0];
+    if (initials.length >= 2) {
+      break;
+    }
+  }
+  return initials;
+}
