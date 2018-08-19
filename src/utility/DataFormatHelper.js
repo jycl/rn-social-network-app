24851e4f39530f9b6a7c294f4227a6f17a826d03
Joshua Leung <joshuaycleung@gmail.com>
2018-08-20 03:38:48 +0800

    Add DataFormatHelper test files and documentation

--- src/utility/DataFormatHelper.js
+++ src/utility/DataFormatHelper.js
@@ -1,3 +1,17 @@
+/**
+ * DataFormatHelper file contains functions for formatting the data
+ * retrieved from the backend API before passing it into UI components
+ * (such as FlatLists) to display.
+ */
+
+/**
+ * Format data (array) so that when it is passed as datainto a FlatList
+ * with the numColumns props, the last row will not be rendered imbalanced
+ * due to last row's elements !== numColumns.
+ * @param {Array} data
+ * @param {number} numberOfCol
+ * @param {Object} fillerObj
+ */
 export function formatDataForGrid(data, numberOfCol, fillerObj = {}) {
   if (data && data.length) {
     const lastRowNumElements = data.length % numberOfCol; //number of elements in last row
@@ -13,6 +27,13 @@ export function formatDataForGrid(data, numberOfCol, fillerObj = {}) {
   }
 }
 
+/**
+ * Retrieve first two initials from a name (excluding parts of the
+ * name that have periods indicating honorifics).
+ * @param {String} name full name to extract initials from
+ * @return {String} first two initials from name, only returns one if
+ *                  name contains one non-honorific word
+ */
 export function getInitialsFromName(name) {
   if (!name) {
     return name;
