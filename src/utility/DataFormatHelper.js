40d5ec310fc9792f4fa8650e91582795fe096d8d
Joshua Leung <joshuaycleung@gmail.com>
2018-08-19 22:24:35 +0800

    Update formatDataForGrid to check data length before parsing

--- src/utility/DataFormatHelper.js
+++ src/utility/DataFormatHelper.js
@@ -1,10 +1,14 @@
 export function formatDataForGrid(data, numberOfCol, fillerObj = {}) {
-  const lastRowNumElements = data.length % numberOfCol; //number of elements in last row
-  let formattedData = data.slice(); //make copy of array to avoid mutating/returning original array
-  if (lastRowNumElements !== 0) {
-    for (let i = lastRowNumElements; i < numberOfCol; i++) {
-      formattedData.push(fillerObj);
+  if (data && data.length) {
+    const lastRowNumElements = data.length % numberOfCol; //number of elements in last row
+    let formattedData = data.slice(); //make copy of array to avoid mutating/returning original array
+    if (lastRowNumElements !== 0) {
+      for (let i = lastRowNumElements; i < numberOfCol; i++) {
+        formattedData.push(fillerObj);
+      }
     }
+    return formattedData;
+  } else {
+    return data;
   }
-  return formattedData;
 }
