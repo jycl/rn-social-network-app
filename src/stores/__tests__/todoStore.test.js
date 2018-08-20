95f96a01c2224c36a8dd99f1ceff7eefb71fd65a
Joshua Leung <joshuaycleung@gmail.com>
2018-08-20 09:08:07 +0800

    Add test for TodoStore adding and clearing data

--- src/stores/__tests__/todoStore.test.js
+++ src/stores/__tests__/todoStore.test.js
@@ -5,8 +5,31 @@ describe("TodoStore tests ", () => {
     expect(todoStore).toMatchSnapshot();
   });
 
-  it("Check initial state values", () => {
+  it("Check initial state values and changes", () => {
     expect(todoStore.rawTodoList).toHaveLength(0);
+    expect(todoStore.todoList).toHaveLength(0);
+
+    const testTodoList = [
+      {
+        id: 1,
+        title: "Jogging",
+        completed: true
+      },
+      {
+        id: 2,
+        title: "Laundry",
+        completed: false
+      }
+    ];
+    todoStore.loadTodoList = jest.fn().mockImplementation(() => {
+      todoStore.rawTodoList = testTodoList;
+    });
+
+    todoStore.loadTodoList("1");
+    expect(todoStore.todoList).toHaveLength(2);
+    expect(todoStore.todoList[0].completed).toBe(true);
+
+    todoStore.clearData();
     expect(todoStore.todoList).toHaveLength(0);
   });
 });
