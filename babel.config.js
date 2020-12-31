2ade7dec2f1a448741e4c83eb45b54201517757d
Joshua Leung <joshuaycleung@gmail.com>
2020-12-31 15:03:11 +0000

    Upgrade to RN0.58 stable build
    Notes for this upgrade:
    - Patch React/Base/RCTModuleMethod.mm (stackoverflow.com/questions/58051510/)
    - Update XCode simulator param in findMatchingSimulator.js
    - Remove polyfill option for @babel/runtime in babel.config.js
    - Ensure JSCore.framework is added to XCode (and bundle ID is defined)
    - Ensure babel/core ^7.0.0 is installed and clean all caches (manually in $TMPDIR, stackoverflow.com/questions/52948363/)

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+    presets: ["module:metro-react-native-babel-preset"],
+    plugins: [
+        [
+            "@babel/plugin-proposal-decorators",
+            {
+            "legacy": true
+            }
+        ],
+        [
+            "@babel/plugin-transform-runtime",
+            {
+            "helpers": true,
+            "regenerator": false
+            }
+        ]
+    ]
+}
