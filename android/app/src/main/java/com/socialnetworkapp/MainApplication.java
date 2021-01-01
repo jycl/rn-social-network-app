c8aa3e7502692ab3626dce54a90e980bab0821db
Joshua Leung <joshuaycleung@gmail.com>
2021-01-01 17:04:53 +0000

    Upgrade to RN0.60 and dependencies
    - Add Gemfile and Podfile, remember to use bundle init, bundle install, bundle exec pod init, bundle exec pod install
    - Unlink and update RNVectorIcons for RN0.60, remove unused fonts
    - Clear cache if encounting RefreshReg error, see: https://github.com/facebook/react-native/issues/25622
    - Add Android SDK path in .bashrc or .zshrc if $ANDROID_SDK_ROOT or $ANDROID)_HOME is not defined (https://github.com/react-native-geolocation/react-native-geolocation/issues/41)

--- android/app/src/main/java/com/socialnetworkapp/MainApplication.java
+++ android/app/src/main/java/com/socialnetworkapp/MainApplication.java
@@ -1,9 +1,12 @@
 package com.socialnetworkapp;
 
 import android.app.Application;
+import android.util.Log;
 
+import com.facebook.react.PackageList;
+import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,10 +25,12 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new VectorIconsPackage()
-      );
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new MyReactNativePackage());
+      // packages.add(new new VectorIconsPackage());
+      return packages;
     }
 
     @Override
